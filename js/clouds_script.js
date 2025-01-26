d6d89721963a8edadda72dced9a7742eb1444a6a
DenMar13 <81777590+DenMar13@users.noreply.github.com>
2025-01-26 13:04:01 +0200

    Version 1.0

---
+++ js/clouds_script.js
@@ -0,0 +1,66 @@
+"use strict"
+const element1 = document.getElementById('cloud1');
+const element2 = document.getElementById('cloud2');
+const element3 = document.getElementById('cloud3');
+const element4 = document.getElementById('cloud4');
+const element5 = document.getElementById('cloud5');
+const element6 = document.getElementById('cloud6');
+const element7 = document.getElementById('cloud7');
+const element8 = document.getElementById('cloud8');
+const element9 = document.getElementById('cloud9');
+const element10 = document.getElementById('cloud10');
+const element11 = document.getElementById('cloud11');
+const element12 = document.getElementById('cloud12');
+const element13 = document.getElementById('cloud13');
+
+element1.onclick = function() { 
+    element1.classList.add('active');
+};
+
+element2.onclick = function() { 
+    element2.classList.add('active');
+};
+
+element3.onclick = function() { 
+    element3.classList.add('active');
+};
+
+element4.onclick = function() { 
+    element4.classList.add('active');
+};
+
+element5.onclick = function() { 
+    element5.classList.add('active');
+};
+
+element6.onclick = function() { 
+    element6.classList.add('active');
+};
+
+element7.onclick = function() { 
+    element7.classList.add('active');
+};
+
+element8.onclick = function() { 
+    element8.classList.add('active');
+};
+
+element9.onclick = function() { 
+    element9.classList.add('active');
+};
+
+element10.onclick = function() { 
+    element10.classList.add('active');
+};
+
+element11.onclick = function() { 
+    element11.classList.add('active');
+};
+
+element12.onclick = function() { 
+    element12.classList.add('active');
+};
+
+element13.onclick = function() { 
+    element13.classList.add('active');
+};
